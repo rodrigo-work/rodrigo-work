8109a590cd420f6470353bf8bcf758654321bd10
Rodrigo Ribeiro <me@rodrigo3d.com>
2025-10-02 05:17:36 -0300

    chore(observability): general updates to the packages/observability

--- packages/observability/status/index.tsx
+++ packages/observability/status/index.tsx
@@ -1,4 +1,5 @@
 import 'server-only'
+
 import { keys } from '../keys'
 import type { BetterStackResponse } from './types'
 
@@ -14,14 +15,11 @@ export const Status = async () => {
   let statusLabel = 'Unable to fetch status'
 
   try {
-    const response = await fetch(
-      'https://uptime.betterstack.com/api/v2/monitors',
-      {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
+    const response = await fetch('https://uptime.betterstack.com/api/v2/monitors', {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
       }
-    )
+    })
 
     if (!response.ok) {
       throw new Error('Failed to fetch status')
@@ -29,9 +27,7 @@ export const Status = async () => {
 
     const { data } = (await response.json()) as BetterStackResponse
 
-    const status =
-      data.filter((monitor) => monitor.attributes.status === 'up').length /
-      data.length
+    const status = data.filter((monitor) => monitor.attributes.status === 'up').length / data.length
 
     if (status === 0) {
       statusColor = 'bg-destructive'
@@ -50,20 +46,18 @@ export const Status = async () => {
 
   return (
     <a
-      className="flex items-center gap-3 text-sm font-medium"
-      target="_blank"
-      rel="noreferrer"
+      className="flex items-center gap-3-- text-sm font-medium"
       href={url}
+      rel="noreferrer"
+      target="_blank"
     >
-      <span className="relative flex h-2 w-2">
+      <span className="relative flex h-2 w-2--">
         <span
           className={`absolute inline-flex h-full w-full animate-ping rounded-full opacity-75 ${statusColor}`}
         />
-        <span
-          className={`relative inline-flex h-2 w-2 rounded-full ${statusColor}`}
-        />
+        <span className={`relative inline-flex h-2 w-2 rounded-full ${statusColor}`} />
       </span>
-      <span className="text-muted-foreground">{statusLabel}</span>
+      <span className={`text-muted-foreground ${statusColor}`}>{statusLabel}</span>
     </a>
   )
 }
